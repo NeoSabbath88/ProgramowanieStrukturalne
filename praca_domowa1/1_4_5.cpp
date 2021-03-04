00b6ed7777c0df61f62ec9ed2f35681140b95eb0
NeoSabbath88 <73527400+NeoSabbath88@users.noreply.github.com>
2021-03-04 16:52:07 +0100

    Add files via upload

--- praca_domowa1/1_4_5.cpp
+++ praca_domowa1/1_4_5.cpp
@@ -1,55 +1,24 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    float a,b;
-    int operacja;
-        cout << "Kalkulator" << endl;
-        cout << "wprowadz pierwsza liczbe" << endl<<endl;
-            cin>>a;
-                cout<<endl;
+    int n,wynik =0;
+    cout << "Podaj nieujemna liczbe calkowita  " << endl;
+    cin>>n;
+    cout<<endl;
+    if(n>=0)
+    {
+        for (int i=0;i<=n;i++)
+        {
+          wynik = wynik+pow(i,2);
 
-        cout << "wprowadz druga liczbe" << endl<<endl;
+        }
 
-            cin>>b;
-                cout<<endl;
-
-
-        cout<< " Operacja: "<<endl;
-        cout<< "1. + "<<endl;
-        cout<< "2. - "<<endl;
-        cout<< "3. * "<<endl;
-        cout<< "4. / "<<endl<<endl;
-
-            cin>>operacja;
-                cout<<endl;
-
-
-    switch(operacja)
-{
-
-
-   case 1:
-    cout <<a<<" + " <<b<<" = "<<a+b<<endl;
-        break;
-
-    case 2:
-  cout <<a<<" - " <<b<<" = "<<a-b<<endl;
-        break;
-
-    case 3:
-  cout <<a<<" * " <<b<<" = "<<a*b<<endl;
-        break;
-
-    case 4:
-  cout <<a<<" / " <<b<<" = "<<a/b<<endl;
-        break;
-
-    default:
-        cout << "Nie wybrano poprawnej operacji"<<endl;
-        break;
-}
+        cout<<"Suma kwadratow liczb od 0 do "<<n<<" : "<<endl<<wynik<<endl;
+    }
+    else
+    cout<<"liczba miala byc nieujemna"<<endl;
     return 0;
 }
